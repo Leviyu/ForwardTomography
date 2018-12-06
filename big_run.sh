efb39b29ec792fdb9003523a8506c1695362a8d6
Ubuntu <ubuntu@ip-172-31-11-141.us-east-2.compute.internal>
2018-12-06 00:49:51 +0000

    p2

--- big_run.sh
+++ big_run.sh
@@ -7,7 +7,7 @@ set PWD = `pwd`
 ## Test Run with Data
 #########################################################
 set ID = T300
-set NUM = 3000
+set NUM = 1000
 set starting_model = S40RTS
 cat $PWD/back/eventinfo.data |head -n ${NUM} > $PWD/LSM_record_input
 sed -i "/MODEL_NAME/c\<MODEL_NAME> ${starting_model}" $PWD/INFILE
