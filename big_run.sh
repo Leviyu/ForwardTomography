e34cc7edc9f7502642b685ae61103edb845d35d1
Ubuntu <ubuntu@ip-172-31-11-141.us-east-2.compute.internal>
2018-12-06 17:44:55 +0000

    d1

--- big_run.sh
+++ big_run.sh
@@ -4,6 +4,20 @@
 set PWD = `pwd`
 
 #########################################################
+## Benchmark test
+#########################################################
+
+set ID = B40
+set NUM = 300000
+set starting_model = EMPTY
+cat $PWD/back/event.CHECK20time |head -n ${NUM} > $PWD/LSM_record_input
+sed -i "/MODEL_NAME/c\<MODEL_NAME> ${starting_model}" $PWD/INFILE
+csh $PWD/mother.sh ${ID} &
+echo "--------------> Working on ID $ID RecordNUM: $NUM starting model: $starting_model"
+sleep 1s
+exit 0
+
+#########################################################
 ## Test Run with Data
 #########################################################
 set ID = T300
@@ -61,18 +75,6 @@ echo "--------------> Working on ID $ID RecordNUM: $NUM starting model: $startin
 sleep 5s
 exit 0
 
-#########################################################
-## Benchmark test
-#########################################################
-
-set ID = B25
-set NUM = 1000
-set starting_model = EMPTY
-cat $PWD/back/event.CHECK20time |head -n ${NUM} > $PWD/LSM_record_input
-sed -i "/MODEL_NAME/c\<MODEL_NAME> ${starting_model}" $PWD/INFILE
-csh $PWD/mother.sh ${ID} &
-echo "--------------> Working on ID $ID RecordNUM: $NUM starting model: $starting_model"
-sleep 1s
 
 ######################################
 set ID = K4
