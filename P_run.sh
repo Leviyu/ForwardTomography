707a03c3dc76fcf6fa10692c35207bda70cb9057
Ubuntu <ubuntu@ip-172-31-11-141.us-east-2.compute.internal>
2018-12-07 22:22:15 +0000

    p1

--- P_run.sh
+++ P_run.sh
@@ -6,10 +6,10 @@ set PWD = `pwd`
 ## Test S Wave
 #########################################################
 
-set ID = J107
-set NUM = 300000
-set iteration = 5
-set starting_model = S40RTS
+set ID = J132
+set NUM = 150000
+set iteration = 3
+set starting_model = GYPSUM_S
 cat $PWD/back/eventinfo.S_Sdiff |head -n ${NUM}  > $PWD/LSM_record_input
 sed -i "/MODEL_NAME/c\<MODEL_NAME> ${starting_model}" $PWD/INFILE
 sed -i "/Iteration_M/c\<Iteration_M> ${iteration}" $PWD/INFILE
@@ -18,15 +18,17 @@ echo "--------------> Working on ID $ID RecordNUM: $NUM starting model: $startin
 sleep 1s
 exit 0
 
+
+
 #########################################################
 ## Test P Wave
 #########################################################
 
-set ID = J103
-set NUM = 50000
+set ID = J122
+set NUM = 15000
 set iteration = 5
-set starting_model = S40RTS
-cat $PWD/back/eventinfo.P.G123 |awk '$3>80 {print $0}' |head -n ${NUM} > $PWD/LSM_record_input
+set starting_model = GYPSUM_P
+cat $PWD/back/eventinfo.P.G123 |awk '$3>80 {print $0}'|head -n ${NUM} > $PWD/LSM_record_input
 sed -i "/MODEL_NAME/c\<MODEL_NAME> ${starting_model}" $PWD/INFILE
 sed -i "/Iteration_M/c\<Iteration_M> ${iteration}" $PWD/INFILE
 sed -i "/dv_type/c\<dv_type> vp" $PWD/INFILE
@@ -35,6 +37,4 @@ echo "--------------> Working on ID $ID RecordNUM: $NUM starting model: $startin
 sed -i "/dv_type/c\<dv_type> vs" $PWD/INFILE
 sleep 1s
 exit 0
-
-
 
